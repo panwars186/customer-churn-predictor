885b537bc05d2c0f73c68b8b03d73c3552358fbd
sandeep <panwars186@gmail.com>
2025-08-04 20:59:03 +0530

    Initial Django project commit

---
+++ myapp/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class HomePageTest(TestCase):
+    def test_homepage_status_code(self):
+        res= self.client.get(reverse('home'))
+        self.assertEqual(res.status_code, 200)
+
+    def test2(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp,"hello world from Django and Jenknins")
